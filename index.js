e8d2d6b5b78126ead2ff1888e282bdbf25790f46
kait brewer <kaitlynmbrewer@gmail.com>
2025-04-15 09:54:15 -0400

    submission for js functions lab

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+function calculateTax(amount) {
+    let taxValue=amount*.1;
+    return taxValue
+}
 
+function convertToUpperCase(text){
+    let upperCaseText=text.toUpperCase();
+    return upperCaseText;
+}
 
+function findMaximum(num1,num2){
+    if (num1>num2){
+        return num1;
+    } else if (num2>num1) {
+        return num2;
+    } else {
+        return num1
+    }
+}
 
+function isPalindrome(word){
+   let reverseWord=word.split("").reverse().join("");
+   if (reverseWord==word){
+    return true;
+   } else {
+    return false;
+   }
+}
 
+function calculateDiscountedPrice(originalPrice,discountPercentage){
+    let discountedPrice=originalPrice-(originalPrice*(discountPercentage/100));
+    return discountedPrice
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
